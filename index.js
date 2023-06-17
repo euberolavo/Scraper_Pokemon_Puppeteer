4b156b2b67374ec0e7474b86548579e4f327e6e9
unknown <euber.olavo.bezerra@gmail.com>
2023-06-17 11:36:24 -0300

    scraper inicial finalizado

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 const puppeteer = require("puppeteer");
 
 const url = "https://pokemon.fandom.com/pt-br/wiki/Pok%C3%A9dex_Nacional";
-const searchFor = "Charizard";
+const searchFor = "Ninetales";
 
 const list = [];
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: 'new' });
   const page = await browser.newPage();
-  console.log("iniciei");
+  console.log("Começou!");
 
+  console.log("Indo para localização do Pokemon...");
   await page.goto(url);
-  console.log("fui para url");
 
   await page.waitForSelector("._2O--J403t2VqCuF8XJAZLK");
   await page.click("._2O--J403t2VqCuF8XJAZLK");
-  console.log("aceitei");
+  console.log("cheguei!");
 
   await page.waitForSelector(".global-navigation__search");
   await page.click(".global-navigation__search");
-  console.log("pesquisa");
+  console.log("Procurando Pokemon...");
 
   await page.waitForSelector(".SearchInput-module_input__LhjJF");
   await page.type(".SearchInput-module_input__LhjJF", searchFor);
+  console.log("Pokemon encontrado!");
 
   await page.waitForSelector(".SearchResultHightlight-module_highlight__t087Y");
   await page.click(".SearchResultHightlight-module_highlight__t087Y");
+  console.log("tentativa de captura...");
 
-  //Pegando informações
+  // Pegando informações
   await page.waitForSelector(".mw-page-title-main");
   const name = await page.$$eval(
     ".mw-page-title-main",
@@ -46,21 +48,32 @@ const list = [];
     (elements) => elements.map((element) => element.innerText)
   );
 
-
   await page.waitForSelector(".modoclaroescuro > table > tbody > tr > td > span > a > font");
   const types = await page.$$eval(
     ".modoclaroescuro > table > tbody > tr > td > span > a > font",
     (elements) => elements.map((element) => element.innerText)
   );
 
+  await page.waitForSelector("#mw-content-text > div > table:nth-child(2) > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(10) > td > table:nth-child(2) > tbody > tr > td > span");
+  const skill = await page.$$eval(
+    "#mw-content-text > div > table:nth-child(2) > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(10) > td > table:nth-child(2) > tbody > tr > td > span",
+    (elements) => elements.map((element) => element.innerText)
+  );
+
+  
+  
   const obj = {};
   obj.name = name;
   obj.types = types;
   obj.height = height;
   obj.weight = weight;
+  obj.skill = skill;
 
   list.push(obj);
+ 
 
+  console.log("gotcha!");
+  console.log("esse é o seu pokemon");
   console.log(obj);
 
   await page.waitForTimeout(6000);
